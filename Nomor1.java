bd6f8a0f4c578a272119ba462673879bbbd74370
Fitriameliasitorus <fitamel86@gmail.com>
2024-10-24 08:33:53 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -2,29 +2,29 @@ import java.util.Scanner;
 
 public class honorLembur {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in) ;
 
-        System.out.print("Masukkan total jam lembur: ");
-        int jamLembur = scanner.nextInt();
+        System.out.print("Masukkan total jam lembur: ") ;
+        int jamLembur = scanner.nextInt() ;
         double honorLembur;
 
 
         if (jamLembur <= 16) {
-            honorLembur = jamLembur * 10000;
+            honorLembur = jamLembur * 10000 ;
         } else {
             // Honor lembur untuk 16 jam pertama
-            double honor16Jam = 16 * 10000;
+            double honor16Jam = 16 * 10000 ;
             // Hitung sisa jam lembur
-            int sisaJam = jamLembur - 16;
+            int sisaJam = jamLembur - 16 ;
             // Honor lembur untuk sisa jam
-            double honorSisaJam = sisaJam * 15000;
+            double honorSisaJam = sisaJam * 15000 ;
             // Total honor lembur
             honorLembur = honor16Jam + honorSisaJam;
         }
 
  
-        System.out.println("Total jam lembur: " + jamLembur);
-        System.out.println("Honor lembur: " + honorLembur);
+        System.out.println("Total jam lembur: " + jamLembur) ;
+        System.out.println("Honor lembur: " + honorLembur) ;
 
         scanner.close();
 }
